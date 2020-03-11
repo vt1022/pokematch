7873cfe4a5d50112c3be88c81860ba4e1a88330a
Vincci Tsui <vinccimantsui@gmail.com>
2020-03-11 17:44:46 -0400

    added cardIsClicked(). game seems to be working perfectly. need to add timer and buttons

--- script/script.js
+++ script/script.js
@@ -1,9 +1,10 @@
 const memApp = {}; // namespace
 
-memApp.amountOfCards = 16; // could add difficulty button to change this
+memApp.amountOfCards = 20; // could add difficulty button to change this
 // -----------------
 // randomize number
 // -----------------
+// create randomizer function
 memApp.randomize = function(maxNumber) {
   return Math.floor(Math.random() * maxNumber) + 1;
 };
@@ -19,7 +20,6 @@ memApp.populateCards = function() {
     const pokemon = memApp.pokemonApi(memApp.randomize(151));
 
     for ( let i = 1; i <=2; i++ ) {
-
       pokemon.then(function(result) {
         const pokemonName = result.name;
         const pokemonImageUrl = result.sprites.front_default;
@@ -51,8 +51,45 @@ memApp.randomizeCardOrder = function() {
   }, 300);
 }
 // -----------------
+// click on cards
+// -----------------
+// event handler on container, then target card overlay
+memApp.cardIsClicked = function() {
+  let clicksInContainer = -1;
+  let clickedPokeName = [];
+  let clickedCard = [];
+  
+  $(".main__cards-container").on("click", ".main__cards-container__card__overlay", function() {
+    clicksInContainer++;
+    $(this).css("width", "0%").css("height", "0%");
+    
+    let accessPrevious = clicksInContainer - 1;
+    clickedCard.push($(this));
+    clickedPokeName.push($(this)[0].nextElementSibling.alt);
+    
+    // every even click, compare clickedPokeName[current] vs [previous] and do some shit
+    if ( (clicksInContainer + 1) % 2 === 0 ) {
+      if (clickedPokeName[clicksInContainer] === clickedPokeName[accessPrevious]) {
+        console.log("they match!");
+
+      } else if (clickedPokeName[clicksInContainer] != clickedPokeName[accessPrevious]) {
+        console.log("they dont match!");
+
+        setTimeout(() => {
+          $(this).css("width", "100%").css("height", "100%");
+          $(clickedCard[accessPrevious]).css("width", "100%").css("height", "100%");
+        }, 600);
+      } else {
+        console.log("stop being safi");
+      }
+    }
+
+  });
+}
+// -----------------
 // pokemon api
 // -----------------
+// save pokemon promise in function
 memApp.pokemonApi = function(pokeId) {
   const pokePromise = $.ajax({
     url: `https://pokeapi.co/api/v2/pokemon/${pokeId}/`,
@@ -67,6 +104,7 @@ memApp.pokemonApi = function(pokeId) {
 // -----------------
 memApp.init = function() {
   memApp.populateCards();
+  memApp.cardIsClicked();
 }
 // -----------------
 // doc ready
