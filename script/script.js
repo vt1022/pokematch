9afecec98cfa25274429b14e1f414bbea3d4b5f8
vt1022 <57807628+vt1022@users.noreply.github.com>
2020-05-23 14:05:33 -0400

    changed a lot of js. fixed a lot of bugs. i dont remember (￣ ‘i ￣;)( *^-^)ρ(*╯^╰)

--- script/script.js
+++ script/script.js
@@ -1,11 +1,3 @@
-// ---------------------------------------------------
-// readme before marking
-// ---------------------------------------------------
-// .js line 166 - reduce timer count for ease of testing
-// main.scss line 26 - add opacity on overlays for ease of testing
-// .js line 56 - encountered a problem I couldn't solve
-// ---------------------------------------------------
-// ---------------------------------------------------
 const memApp = {}; // namespace
 
 memApp.timerCount = 0; 
@@ -28,10 +20,8 @@ memApp.shuffleArray = function(a) {
 // -----------------
 // populate cards
 // -----------------
-// take img src as argument
-// write function to populate cards with for loop
-// append html of cards to container
 memApp.populateCards = function() {
+  $("footer").hide();
   $(".main__cards-container").empty();
   memApp.shuffleArray(memApp.pokemonsFromApi);
   // take the first X pokemon from the shuffled array where X is half of the game cards amount
@@ -40,6 +30,7 @@ memApp.populateCards = function() {
   pokemonCards.push(...pokemonCards);
   memApp.shuffleArray(pokemonCards);
   
+  // append html of cards to container
   pokemonCards.forEach((pokemonObject) => {
     const { name, image } = pokemonObject;
 
@@ -59,48 +50,58 @@ memApp.populateCards = function() {
   });
 }
 // -----------------
+// global function and variables for reusable code
+// -----------------
+memApp.clicksInContainer = -1;
+memApp.matchedCards = 0;
+memApp.clickedCard = [];
+memApp.clickedPokeName = [];
+memApp.resetTrackers = function() {
+  this.clicksInContainer = -1;
+  this.clickedPokeName.length = 0;
+  this.clickedCard.length = 0;
+  this.matchedCards = 0;
+  $(".header__list__item__timer").empty();
+}
+// -----------------
 // click on cards
 // -----------------
 // event handler on container, then target card inner
 memApp.cardIsClicked = function() {
-  let clicksInContainer = -1;
-  const clickedPokeName = [];
-  const clickedCard = [];
-  let matchedCards = 0;
-  
   $(".main__cards-container").on("click", ".main__cards-container__inner", function() {
-    clicksInContainer++;
-    const $this = $(this); // used on ln 102 prevents clicking too fast bug?
-    let accessPrevious = clicksInContainer - 1;
-    console.log(clickedPokeName);
-    console.log(clickedCard);
-    
-    $(this).toggleClass("flip-card");
-    clickedCard.push($(this));
-    clickedPokeName.push($(this).attr("aria-label"));
+    memApp.clicksInContainer++;
+    const $this = $(this); // used on ln 111 prevents clicking too fast bug?
+    let accessPrevious = memApp.clicksInContainer - 1;
+    memApp.clickedCard.push($(this));
+    memApp.clickedPokeName.push($(this).attr("aria-label"));
+    $(this).toggleClass("flip-card").attr("disabled", true);
     
     // every even click, compare clickedPokeName[current] vs [previous] and do some shit
-    if ( (clicksInContainer + 1) % 2 === 0 ) {
-      if (clickedPokeName[clicksInContainer] === clickedPokeName[accessPrevious] && matchedCards === (memApp.amountOfCards - 2)) {
+    if ( (memApp.clicksInContainer + 1) % 2 === 0 ) {
+      if (memApp.clickedPokeName[memApp.clicksInContainer] === memApp.clickedPokeName[accessPrevious] && memApp.matchedCards === (memApp.amountOfCards - 2)) {
         // if won:
-        clearInterval(timer);
+        clearInterval(memApp.timer);
         const wonHtml = `
         You WON! You managed to MATCH all the Pokémon! CLICK start to test your SKILLS again!
         <button class="footer__landing__welcome__button"><span>&#9654</span>START</button>`;
         
         $(".footer__landing__welcome p").html(wonHtml);
-        $(".footer").show();
-        resetTrackers();
-
-      } else if (clickedPokeName[clicksInContainer] === clickedPokeName[accessPrevious] && matchedCards < memApp.amountOfCards) {
-        // if cards match but didn't win yet:
-        matchedCards+= 2;
-      } else if (clickedPokeName[clicksInContainer] != clickedPokeName[accessPrevious]) {
-        // cards don't match:
         setTimeout(function() {
-          // flip cards back:
-          $this.toggleClass("flip-card");
-          $(clickedCard[accessPrevious]).toggleClass("flip-card");
+          $(".footer").show();
+          memApp.resetTrackers();
+        }, 1000);
+
+      } else if (memApp.clickedPokeName[memApp.clicksInContainer] === memApp.clickedPokeName[accessPrevious] && memApp.matchedCards < memApp.amountOfCards) {
+        // if cards match but didn't win yet:
+        memApp.matchedCards+= 2;
+        $this.find(".main__cards-container__card__back").toggleClass("matched");
+        $(memApp.clickedCard[accessPrevious]).find(".main__cards-container__card__back").toggleClass("matched");
+        
+      } else if (memApp.clickedPokeName[memApp.clicksInContainer] != memApp.clickedPokeName[accessPrevious]) {
+        // cards don't match:
+        setTimeout(function() { // flip cards back:
+          $this.toggleClass("flip-card").attr("disabled", false);
+          $(memApp.clickedCard[accessPrevious]).toggleClass("flip-card").attr("disabled", false);
         }, 600);
 
       } else { // error
@@ -112,61 +113,11 @@ memApp.cardIsClicked = function() {
         $(".footer").show();
         alert("error! safi, please stop breaking my shit");
         console.log("error! safi, please stop breaking my shit");
-        resetTrackers();
+        memApp.resetTrackers();
       }
+      $this.blur();
     }
   });
-
-  // reset and start button click event:
-  $(".header__list__item__reset, .footer__landing__welcome")
-  .on("click", ".footer__landing__welcome__button", function(e) {
-    e.preventDefault();
-    // clicking reset/start button brings user back to landing
-    memApp.populateCards();
-      
-    // timer
-    memApp.timerCount = 40; 
-    $(".header__list__item__timer").html(memApp.timerCount+'s');
-  
-    const timer = setInterval(function() {
-      memApp.timerCount--;
-  
-      if (memApp.timerCount > 0) {
-        $(".header__list__item__timer").html(memApp.timerCount+'s');
-        
-      } else if (memApp.timerCount <= 0) {
-        const lostHtml = `
-        You ran OUT of TIME! Better luck NEXT time. CLICK start to try again!
-        <button class="footer__landing__welcome__button"><span>&#9654</span>START</button>`;
-        
-        $(".footer__landing__welcome p").html(lostHtml);
-        $(".footer").show();
-        clearInterval(timer);
-        resetTrackers();
-  
-      } else {
-        const errorHtml = `
-        Sorry, there was an error on our end. Please try again!
-        <button class="footer__landing__welcome__button"><span>&#9654</span>START</button>`;
-        
-        $(".footer__landing__welcome p").html(errorHtml);
-        $(".footer").show();
-        clearInterval(timer);
-        alert("error! safi, please stop breaking my shit");
-        console.log("error! safi, please stop breaking my shit");
-        resetTrackers();
-      }
-      
-    }, 1000); // const timer
-    $("footer").hide();
-  }); // reset and start button click event
-
-  const resetTrackers = function() {
-    clicksInContainer = -1;
-    clickedPokeName.length = 0;
-    clickedCard.length = 0;
-    matchedCards = 0;
-  }
 }
 // -----------------
 // pokemon api promise
@@ -202,6 +153,44 @@ memApp.apiDataToLocal = function() {
 memApp.init = function() {
   memApp.apiDataToLocal();
   memApp.cardIsClicked();
+  // reset and start button click event:
+  $(".header__list__item__reset, .footer__landing__welcome")
+  .on("click", ".footer__landing__welcome__button", function() {
+    memApp.populateCards();
+    memApp.timerCount = 40; 
+
+    memApp.timer = setInterval(function() {
+      $(".header__list__item__timer").html(memApp.timerCount+'s');
+      memApp.timerCount--;
+
+      if (memApp.timerCount > 0) {
+        $(".header__list__item__timer").html(memApp.timerCount+'s');
+        
+      } else if (memApp.timerCount <= 0) {
+        const lostHtml = `
+        You ran OUT of TIME! Better luck NEXT time. CLICK start to try again!
+        <button class="footer__landing__welcome__button"><span>&#9654</span>START</button>`;
+        
+        $(".footer__landing__welcome p").html(lostHtml);
+        $(".footer").show();
+        clearInterval(memApp.timer);
+        memApp.resetTrackers();
+
+      } else {
+        const errorHtml = `
+        Sorry, there was an error on our end. Please try again!
+        <button class="footer__landing__welcome__button"><span>&#9654</span>START</button>`;
+        
+        $(".footer__landing__welcome p").html(errorHtml);
+        $(".footer").show();
+        clearInterval(memApp.timer);
+        alert("error! safi, please stop breaking my shit");
+        console.log("error! safi, please stop breaking my shit");
+        memApp.resetTrackers();
+
+      }
+    }, 1000); // const timer
+  }); // reset and start button listener
 }
 // -----------------
 // doc ready
